450a823cead21d07335064c016d8ea423f91cab1
haruhide39 <sc91rdkpsng3dbuneeew@docomo.ne.jp>
2025-02-14 22:25:41 +0900

    React 導入

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>初めてのReactアプリケーション</h1>
+      <p>Reactを使って簡単なアプリを作成しています。</p>
+    </div>
+  );
+}
+
+export default App;
